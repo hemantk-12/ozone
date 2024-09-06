966302f934fc18672f1d0a588212971b8064d033
Wei-Chiu Chuang <weichiu@apache.org>
2024-09-05 21:56:52 -0700

    CDPD-74032. HDDS-11391. Addendum to fix test failure.
    Change-Id: I63af2c4af9187eab15b0b08acdd9f62686e64993
    (cherry picked from commit 7a452ca2e84520c2fb15356fde38857cacb758c5)

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/TestHSync.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/TestHSync.java
@@ -428,6 +428,8 @@ public class TestHSync {
   }
 
   @Test
+  // Making this the second test to be run to avoid lingering block files from previous tests
+  @Order(2)
   public void testKeyHSyncThenClose() throws Exception {
     // Check that deletedTable should not have keys with the same block as in
     // keyTable's when a key is hsync()'ed then close()'d.
