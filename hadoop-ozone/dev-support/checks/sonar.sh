d4ff453b0a0e230daf3752d1396c08819627b940
tanvipenumudy <tanvi.penumudy@cloudera.com>
2025-03-12 09:51:05 +0530

    CDPD-78788. Modify sonar.sh to push changes to internal SonarQube (#250)

--- hadoop-ozone/dev-support/checks/sonar.sh
+++ hadoop-ozone/dev-support/checks/sonar.sh
@@ -26,5 +26,5 @@ fi
 
 mvn -V -B -DskipShade -DskipTests -Dskip.npx -Dskip.installnpx --no-transfer-progress \
   -Dsonar.coverage.jacoco.xmlReportPaths="$(pwd)/target/coverage/all.xml" \
-  -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=apache -Dsonar.projectKey=hadoop-ozone \
-  verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar
+  -Dsonar.host.url=https://sonarqube.infra.cloudera.com -Dsonar.projectKey=ozone_ut_cdh_main \
+  -Dsonar.projectName=Ozone_UT verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar
